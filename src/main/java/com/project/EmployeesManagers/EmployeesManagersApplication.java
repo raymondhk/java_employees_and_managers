888af4c3754ff0adf7a240e193f5d66d72173bf0
Raymond Kim <ray.kim09@gmail.com>
2017-11-11 21:35:29 -0500

    init commit

---
+++ src/main/java/com/project/EmployeesManagers/EmployeesManagersApplication.java
@@ -0,0 +1,11 @@
+package com.project.EmployeesManagers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesManagersApplication{
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesManagersApplication.class, args);
+	}
+}
---
+++ src/main/java/com/project/EmployeesManagers/models/Employee.java
@@ -0,0 +1,97 @@
+package com.project.EmployeesManagers.models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.validator.constraints.Email;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="employees")
+public class Employee{
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String firstName;
+	private String lastName;
+	@Column(updatable=false)
+	@DateTimeFormat(pattern="MM:dd:yyyy HH:mm:ss")
+	private Date createdAt;
+	
+	@DateTimeFormat(pattern="MM:dd:yyyy HH:mm:ss")
+	private Date updatedAt;
+
+	@PrePersist
+	public void onCreate(){this.createdAt = new Date();}
+	@PreUpdate
+	public void onUpdate(){this.updatedAt = new Date();}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	// Setters and Getters go here
+
+	public Employee(){}
+	
+	public Employee(){
+
+		this.createdAt = new Date();
+		this.updatedAt = new Date();
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
